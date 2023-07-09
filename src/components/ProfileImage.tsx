532bbae51806794dd6ccd6ffc49bdac2b52e00d6
Rosalie Newman <rosalienewman0@gmail.com>
2023-07-09 17:04:46 -0400

    profile image from discord displays

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
-export default function ProfileImage({ src }) {
-  return <div>ProfileImage</div>;
+type ProfileImageProps = {
+  src?: string | null;
+  className?: string;
+};
+export default function ProfileImage({
+  src,
+  className = "",
+}: ProfileImageProps) {
+  return (
+    <div
+      className={`relative h-12 w-12 overflow-hidden rounded-full ${className}`}
+    >
+      {src == null ? null : (
+        <Image src={src} alt="profile image" quality={100} fill />
+      )}
+    </div>
+  );
 }
