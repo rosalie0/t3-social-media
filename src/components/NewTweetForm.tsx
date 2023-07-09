ee3c6c93a32f0ecb651a03c601b63b531dbbb321
Rosalie Newman <rosalienewman0@gmail.com>
2023-07-09 17:21:52 -0400

    tweet form made

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useCallback, useLayoutEffect, useRef, useState } from "react";
 import ProfileImage from "./ProfileImage";
 import Button from "./Button";
 import { useSession } from "next-auth/react";
 
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
+
 export default function NewTweetForm() {
   const session = useSession();
+  if (session.status !== "authenticated") return null;
+  // Only call the Form (which contains the useLayoutEffect) if we are auth'd.
+  // terminal complains if useLayoutEffect is called possibly without any jsx.
+  return <Form />;
+}
+
+export function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
   // if user isn't authd, dont display the form.
   if (session.status !== "authenticated") {
     return;
   }
+
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
-      <div className="flex gap-4"></div>
-      <ProfileImage src={session.data.user.image} />
-      <textarea
-        className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
-        placeholder="What's happening?"
-      />
+      <div className="flex gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's happening?"
+        />
+      </div>
       <Button className="self-end">Tweet</Button>
     </form>
   );
